4365b5d0cdb20b59178676ff8c1951a05e12dc33
Oliver Brenner <oliver@hackzurich.com>
2019-09-28 12:59:17 +0200

    fixed front end get request.

--- Front-End/src/app/tabs/tabs.page.ts
+++ Front-End/src/app/tabs/tabs.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
 
--- Front-End/src/app/tabs/tabs.router.module.ts
+++ Front-End/src/app/tabs/tabs.router.module.ts
@@ -38,16 +38,6 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab4',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab4/tab4.module').then(m => m.Tab4PageModule)
-          }
-        ]
-      },
-      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
