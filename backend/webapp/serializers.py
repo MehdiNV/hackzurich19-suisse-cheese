61eaab13e32fec83c9df959016cc428c9587cc61
Oliver Brenner <oliver@hackzurich.com>
2019-09-29 00:50:38 +0200

    Cleaned serializer.

--- backend/webapp/serializers.py
+++ backend/webapp/serializers.py
@@ -5,6 +5,4 @@ from .models import Articles
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Articles
-        # fields = ('title', 'journal', 'timestamp', 'body')
-        # fields = ('title')
         fields = '__all__'
