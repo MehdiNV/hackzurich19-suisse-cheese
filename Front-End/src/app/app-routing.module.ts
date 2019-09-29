862559497d612fce643450aabebc09e94403a099
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2019-09-29 07:57:28 +0200

    Refined pages

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  { path: 'article', loadChildren: './article/article.module#ArticlePageModule' },
+  }
 ];
 @NgModule({
   imports: [
